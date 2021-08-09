1c9c2d946fada2b2a246e7ee6d4883beb777a085
Mansi Arora <43355695+mansiarora05@users.noreply.github.com>
2021-08-09 08:26:21 -0400

    Update RunVerifyBamID.sh

--- RunVerifyBamID.sh
+++ RunVerifyBamID.sh
@@ -1,16 +1,13 @@
 ##!/bin/bash
-##SBATCH --job-name=create_vcf
+##SBATCH --job-name=RunVerifyBamID
 ##BATCH --nodes=1
 ##SBATCH --ntasks=1
 ##SBATCH --cpus-per-task=1
-##BATCH --time=2:00:00
-
-##module load bcftools
+##BATCH --time=10:00:00
 
 
-for i in {1..5}; do
-	sbatch --job-name=${i}_VerifyBamID --account=rrg-vmooser --nodes=1 --wrap="/home/mansi/projects/rrg-vmooser/shared/tools/verifyBamID_2.0.1/VerifyBamID.Linux.x86-64 --UDPath /home/mansi/projects/rrg-vmooser/shared/tools/verifyBamID_2.0.1/VerifyBamID/resource/1000g.phase3.100k.b38.vcf.gz.dat.UD --BedPath /home/mansi/projects/rrg-vmooser/shared/tools/verifyBamID_2.0.1/VerifyBamID/resource/1000g.phase3.100k.b38.vcf.gz.dat.bed --MeanPath /home/mansi/projects/rrg-vmooser/shared/tools/verifyBamID_2.0.1/VerifyBamID/resource/1000g.phase3.100k.b38.vcf.gz.dat.mu --Reference /lustre03/project/6060121/dtaliun/BCQ19/Reference/Homo_sapiens.GRCh38.fa --BamFile /lustre03/project/6060121/dtaliun/BCQ19/CRAM_test/BQC*.sorted.dup.recal.cram"  -o slurm__${i}.log
+for i in /home/mansi/projects/rrg-vmooser/dtaliun/BQC19/Initial_Internal_QC_Release/cram/BQC*.sorted.dup.recal.cram; do
+        file_name="${i##*/}"
+        file="${file_name%.*}"
+        sbatch --job-name=${file}_VerifyBamID --account=rrg-vmooser --nodes=1  --ntasks-per-node=1 --cpus-per-task=1 --mem-per-cpu=4000 --time=10:00:00 --wrap="/home/mansi/projects/rrg-vmooser/shared/tools/verifyBamID_2.0.1/VerifyBamID.Linux.x86-64 --UDPath /home/mansi/projects/rrg-vmooser/shared/tools/verifyBamID_2.0.1/VerifyBamID/resource/1000g.phase3.100k.b38.vcf.gz.dat.UD --BedPath /home/mansi/projects/rrg-vmooser/shared/tools/verifyBamID_2.0.1/VerifyBamID/resource/1000g.phase3.100k.b38.vcf.gz.dat.bed --MeanPath /home/mansi/projects/rrg-vmooser/shared/tools/verifyBamID_2.0.1/VerifyBamID/resource/1000g.phase3.100k.b38.vcf.gz.dat.mu --Reference /lustre03/project/6060121/dtaliun/BCQ19/Reference/Homo_sapiens.GRCh38.fa --BamFile ${i} --Output ${file}.prefix"  -o slurm_${file}.log
 done
-
-
-#./VerifyBamID.Linux.x86-64 --UDPath /home/mansi/projects/rrg-vmooser/shared/tools/verifyBamID_2.0.1/VerifyBamID/resource/1000g.phase3.100k.b38.vcf.gz.dat.UD --BedPath /home/mansi/projects/rrg-vmooser/shared/tools/verifyBamID_2.0.1/VerifyBamID/resource/1000g.phase3.100k.b38.vcf.gz.dat.bed --MeanPath /home/mansi/projects/rrg-vmooser/shared/tools/verifyBamID_2.0.1/VerifyBamID/resource/1000g.phase3.100k.b38.vcf.gz.dat.mu --Reference /lustre03/project/6060121/dtaliun/BCQ19/Reference/Homo_sapiens.GRCh38.fa --BamFile /lustre03/project/6060121/dtaliun/BCQ19/CRAM_test/BQC10037.sorted.dup.recal.cram
